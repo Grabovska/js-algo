49b82965c471d11c8de78b65bf6779e0ccd66816
Юлія Грабовська <Grabovska-1355@KPNU.KM.UA>
2017-04-18 17:28:00 +0300

    Step 01

---
+++ app.js
@@ -0,0 +1,8 @@
+function writeAlgoDemo() {
+    var side_a = Math.random() * 10 + +5;
+    var side_b = Math/random() * 10 + +5;
+     document.write("side_a=" + side_a + "Side_b=" + +side_b);
+     document.write("Об'єм куба" + side_a * side_a * side_a);
+     document.write("Площадь квадрата" + side_a * side_a);
+     document.write("Площадь треугольника" + side_a * side_b);
+}
